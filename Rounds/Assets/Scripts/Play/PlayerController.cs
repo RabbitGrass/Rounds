c7419c645947513ae9dd3fb22f97624167ad90a8
RabbitGrass <145101277+RabbitGrass@users.noreply.github.com>
2025-08-06 15:59:38 +0900

    캐릭터 점프 및 이동 커밋

--- Rounds/Assets/Scripts/Play/PlayerController.cs
+++ Rounds/Assets/Scripts/Play/PlayerController.cs
@@ -6,78 +6,75 @@ public class PlayerController : MonoBehaviour
 {
     public Rigidbody2D rb;
     public float speed;
+    public float jumpPower;
 
     private bool jumpAble = false;
     Vector2 direction;
 
     public Transform groundCheckTransform;
+    public LayerMask groundCheckLayer;
     private bool grounded;
 
-    private void FixedUpdate()
-    {
-        Vector2 vector = transform.position;
-        Movement(vector);
-        UpdateGroundedStatus();
-    }
+    public Transform wallCheckTransform;
+    private bool isWall;
 
-    private void Movement(Vector2 vector)
+    private void Update()
     {
-        if (Input.GetKey(KeyCode.D))
+        if (jumpAble && Input.GetKeyDown(KeyCode.Space)) //GetKeyDown은 반드시 Update에서 써야한다.
         {
-            vector.x += speed;
-            rb.AddForce(vector);
-        }
-        
-        if (Input.GetKey(KeyCode.A))
-        {
-            vector.x -= speed;
-            rb.AddForce(vector);
-        }
-
-        if (jumpAble && Input.GetKey(KeyCode.Space))
-        {
-            if (grounded)
+            if (isWall)
             {
-                vector.y += 150;
+                //vector.y += jumpPower;
+                //rb.AddForce(Vector2.);
             }
-            else if (!grounded)
-            {
-                vector.y += 130;
-                rb.AddForce(direction);
-            }
-            rb.AddForce(vector);
+            rb.AddForce(Vector2.up * jumpPower, ForceMode2D.Impulse);
         }
     }
 
-    private void UpdateGroundedStatus()
+    private void FixedUpdate() //fixedUpdate는 주로 물리엔진과 연관이 된 애들만 넣는다.
     {
-        grounded = Physics2D.OverlapCircle(groundCheckTransform.position, 0.001f);
-        
+        float h = Input.GetAxis("Horizontal");
+        Debug.Log(jumpAble);
+
+        Vector2 vector = rb.velocity;
+
+        //transform.Translate(vector * speed * Time.deltaTime);
+
+        vector.x = h * speed;
+        rb.velocity = vector;
+        UpdateJumpAbleStatus();
     }
 
-    private void OnCollisionEnter2D(Collision2D collision)
+    private void UpdateJumpAbleStatus()
     {
-        jumpAble = true;
-        Debug.Log("jump : " + jumpAble);
-        Debug.Log("ground : " + grounded);
-        if (!grounded)
-        {
-            direction = collision.gameObject.transform.position - transform.position;
-            direction = direction.normalized * (-100);
-        }
+        grounded = Physics2D.OverlapCircle(groundCheckTransform.position, 0.001f, groundCheckLayer);
+        isWall = Physics2D.OverlapCircle(groundCheckTransform.position, 0.1f, groundCheckLayer);
     }
+
+    //private void OnCollisionEnter2D(Collision2D collision)
+    //{
+    //    //if (!grounded)
+    //    //{
+    //    //    direction = collision.gameObject.transform.position - transform.position;
+    //    //    direction = direction.normalized * (-jumpPower);
+    //    //}
+    //}
 
     private void OnCollisionStay2D(Collision2D collision)
     {
-        if (grounded && !jumpAble)
+        if ((isWall || grounded) && !jumpAble)
         {
             jumpAble = true;
+        }
+        if (isWall)
+        {
+            //rb.gravityScale = 0.5f;
         }
     }
 
     private void OnCollisionExit2D(Collision2D collision)
     {
-         jumpAble = false;
-         Debug.Log("jump : " + jumpAble);
+        jumpAble = false;
+        rb.gravityScale = 1;
     }
 }
