2f68548fd326ca4f3596e105d0145f144668e6ba
RabbitGrass <145101277+RabbitGrass@users.noreply.github.com>
2025-08-07 17:35:26 +0900

    Effect포토샵

--- Rounds/Assets/Scripts/Play/PlayerController.cs
+++ Rounds/Assets/Scripts/Play/PlayerController.cs
@@ -6,6 +6,10 @@ public class PlayerController : MonoBehaviour
 {
     public Rigidbody2D rb;
     public float speed;
+    private float wallJumpSpeed;
+    private bool isWallJumping = false;
+    public float wallJumpDuration;
+    private float wallJumpTimer = 0f;
     public float jumpPower;
 
     private bool jumpAble = false;
@@ -16,28 +20,50 @@ public class PlayerController : MonoBehaviour
 
     public Transform wallCheckRight;
     public Transform wallCheckLeft;
-    private bool isWall;
+    private bool isWallRight = false;
+    private bool isWallLeft = false;
+
+    private void Start()
+    {
+        wallJumpSpeed = speed * 0.5f;
+    }
 
     private void Update()
     {
-        Debug.Log("벽 : " + isWall);
-        Debug.Log("점프 : " + jumpAble);
         if (jumpAble && Input.GetKeyDown(KeyCode.Space)) //GetKeyDown은 반드시 Update에서 써야한다.
         {
-            if (isWall && !grounded)
-            {
-                //벽 점프: x는 벽 반대 방향, y는 위
-                float wallJumpDirection = transform.localScale.x > 0 ? -1 : 1;
-                Vector2 jumpDir = new Vector2(wallJumpDirection, 1).normalized;
-
-                //rb.velocity = Vector2.zero;//기존 속도 초기화
-                rb.AddForce(jumpDir * jumpPower, ForceMode2D.Impulse);
-            }
-            else if (grounded)
+            float wallJumpDirection = 0;
+            if (grounded)
             {
                 rb.AddForce(Vector2.up * jumpPower, ForceMode2D.Impulse);
 
             }
+            else if (!grounded)
+            {
+                if (isWallRight)
+                {
+                    //벽 점프: x는 벽 반대 방향, y는 위
+                    wallJumpDirection = -1;
+                }
+                else if (isWallLeft)
+                {
+                    wallJumpDirection = 1;
+                }
+
+                //rb.velocity = Vector2.zero;                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                     
+                //Vector2 jumpDir = new Vector2(wallJumpDirection, 1).normalized;
+                //rb.AddForce(jumpDir * jumpPower, ForceMode2D.Impulse);
+                rb.velocity = new Vector2(wallJumpDirection * wallJumpSpeed, jumpPower);
+                isWallJumping = true;
+                wallJumpTimer = wallJumpDuration;
+            }
+        }
+
+        if (isWallJumping)
+        {
+            wallJumpTimer -= Time.deltaTime;
+            if (wallJumpTimer <= 0)
+                isWallJumping = false;
         }
     }
 
@@ -46,44 +72,32 @@ public class PlayerController : MonoBehaviour
         float h = Input.GetAxis("Horizontal");
 
         Vector2 vector = rb.velocity;
-
-        //transform.Translate(vector * speed * Time.deltaTime);
-
         vector.x = h * speed;
-        rb.velocity = vector;
+
+
+        if ((!isWallRight && !isWallLeft && !isWallJumping) || grounded)
+            rb.velocity = vector;
+
         UpdateJumpAbleStatus();
+
+        SlideWall();
     }
 
     private void UpdateJumpAbleStatus()
     {
         grounded = Physics2D.OverlapCircle(groundCheckTransform.position, 0.001f, groundCheckLayer);
-        isWall = Physics2D.OverlapCircle(wallCheckRight.position, 0.01f, groundCheckLayer) || Physics2D.OverlapCircle(wallCheckRight.position, 0.01f, groundCheckLayer);
+        isWallRight = Physics2D.OverlapCircle(wallCheckRight.position, 0.01f, groundCheckLayer);
+        isWallLeft = Physics2D.OverlapCircle(wallCheckLeft.position, 0.01f, groundCheckLayer);
+
+        jumpAble = grounded || isWallLeft || isWallRight;
     }
 
-    //private void OnCollisionEnter2D(Collision2D collision)
-    //{
-    //    //if (!grounded)
-    //    //{
-    //    //    direction = collision.gameObject.transform.position - transform.position;
-    //    //    direction = direction.normalized * (-jumpPower);
-    //    //}
-    //}
-
-    private void OnCollisionStay2D(Collision2D collision)
+    private void SlideWall()
     {
-        if ((isWall || grounded) && !jumpAble)
+        if (!grounded && (isWallLeft || isWallRight))
         {
-            jumpAble = true;
-        }
-        if (isWall)
-        {
-            //rb.gravityScale = 0.5f;
+            rb.velocity = new Vector2(rb.velocity.x, Mathf.Clamp(rb.velocity.y, -2f, float.MaxValue)); // 느리게 떨어지기
         }
     }
 
-    private void OnCollisionExit2D(Collision2D collision)
-    {
-        jumpAble = false;
-        rb.gravityScale = 1;
-    }
 }
